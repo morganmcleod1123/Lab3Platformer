adad1dfd8e00c89bc1d1ca3abe5ea118cb3d279d
Josh <thomeczekje@hendrix.edu>
2021-02-22 22:30:02 -0600

    Added tileset. Cannot match size with grid >:(

---
+++ Lab3Platformer/Assets/Scripts/Annotations.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Tileset: https://hello-tazzina.itch.io/greenwoods-ii
+
+
+
+
+
+public class Annotations : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
