9a1071a0f36d269963b274c85f7c1c20ab1a1568
Josh <thomeczekje@hendrix.edu>
2021-02-25 12:40:21 -0600

    Start Menu

--- Lab3Platformer/Assets/Scripts/GameManager.cs
+++ Lab3Platformer/Assets/Scripts/GameManager.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
     public Vector3 lastCheckpointPos;
     public Vector3 gameStartPoint;
+
+    public GameObject startButton;
+    public GameObject backgroundImage;
 
     private void Awake()
     {
@@ -29,5 +33,35 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void StartButton()
+    {
+        startButton.SetActive(false);
+        StartCoroutine(ColorLerp(new Color(1, 1, 1, 0), 4));
+    }
+
+    public void GameOver()
+    {
+        startButton.SetActive(true);
+        StopAllCoroutines();
+        //HideDialog();
+        StartCoroutine(ColorLerp(new Color(1, 1, 1, 1), 1));
+
+    }
+
+    IEnumerator ColorLerp(Color endValue, float duration)
+    {
+        float time = 0;
+        Image sprite = backgroundImage.GetComponent<Image>();
+        Color startValue = sprite.color;
+
+        while(time < duration)
+        {
+            sprite.color = Color.Lerp(startValue, endValue, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        sprite.color = endValue;
     }
 }
